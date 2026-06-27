8c8f5bcc8654f6567f191b078f8c5a9ea23bc2ad
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2026-06-27 15:18:18 +0200

    Models done

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:expense_tracker/expenses.dart';
 
 void main() {
-  runApp(MaterialApp(home: Expenses()));
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
 }
