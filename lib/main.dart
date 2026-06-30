676266e7450ffed097aa01f1091035d715897949
Daffnel <3ggapp24.andrda@folkuniversitetet.nu>
2026-06-30 16:24:09 +0200

    Theme settings done

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:expense_tracker/theme/color_schemes.g.dart';
+
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData(useMaterial3: true, colorScheme: MaterialTheme.lightScheme()),
+      darkTheme: ThemeData(useMaterial3: true, colorScheme: MaterialTheme.darkScheme()),
       home: Expenses(),
     ),
   );
